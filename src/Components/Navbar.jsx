dc052dcb42eafd5b53ab70a565c653efd5220a1a
Gourav Makode <makodegourav1@gmail.com>
2025-06-06 12:33:24 +0530

    2nd todo push

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,22 +3,23 @@ import { CgProfile } from "react-icons/cg";
 import profile from '../Assets/profile.png';
 import bell from '../Assets/bell-svgrepo-com.svg';
 import { Link } from 'react-router-dom';
+import { FaBell } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
         <header className="flex justify-between items-center p-5">
-        <div className="text-2xl font-[cursive]">study-sync</div>
+        <div className="text-4xl font-bold font-[cursive]">study-sync</div>
         <nav className="flex space-x-2">
           <button className="bg-[#c9a974] hidden md:block text-sm px-3 py-2 rounded-lg"><Link to="/Productivity">productivity</Link></button>
           <button className="bg-[#c9a974] hidden md:block text-sm px-3 py-2 rounded-lg"><Link to="/Collaboration">collaboration</Link></button>
           <button className="bg-[#c9a974] hidden md:block text-sm px-3 py-2 rounded-lg"><Link to="/Pricing">pricing</Link></button>
         </nav>
         <div className="flex items-center space-x-4">
-          <div className="w-6 h-6 bg-[#c9a974] rounded-full flex items-center justify-center">
-            <img src={bell} alt="Notification Bell" className="w-4 h-4" />
+          <div className="rounded-full flex items-center justify-center">
+            <FaBell className="w-7 h-7 rotate-45 text-cyan-300" />
           </div>
-          <button className="bg-gray-500 text-white px-3 py-2 rounded-full">log in</button>
+          <button className="bg-gray-500 text-white px-3 py-2 rounded-3xl">log in</button>
           <img src={profile} alt="User Icon" className="w-10 h-10 rounded-full ml-2" />
         </div>
         </header>
